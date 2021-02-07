a8291200531c554a41ccead9cb68124d3c0b40e4
Seiran-M <seyran.m1@gmail.com>
2021-02-07 18:50:20 +0300

    added remaining files

---
+++ src/components/Select/Select.tsx
@@ -0,0 +1,1 @@
+import React from "react";type ItemType={   title:string   value:any}export type SelectPropsType = {   value: any   onChange: (value:any) => void   items: ItemType[]}export function Select(props: SelectPropsType) {   return (      <div>      </div>   )}
